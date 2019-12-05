0c8394925ad0f8ca3c888b1e0bf3f596265de6e7
Nico Arato <nico.arato@gmail.com>
2019-12-04 21:32:47 -0300

    Modulos en python

---
+++ modulo2-consola.py
@@ -0,0 +1,10 @@
+""" import modulo2 as m2
+
+m2.chau("Maria")
+m2.saludar("Maria") """
+
+import camelcase
+camel = camelcase.CamelCase()
+
+texto = "mi nombre es antonio"
+print(camel.hump(texto))
---
+++ modulo2.py
@@ -0,0 +1,5 @@
+def saludar(nombre):
+    print("Hola soy "+ nombre)
+
+def chau(nombre):
+    print("Adios "+ nombre)
